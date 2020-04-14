69ccb85fd107487a56b5821a6ec7dec51a8cfa20
deyren <deyren4@gmail.com>
2020-04-14 19:43:45 +0200

    probar tests

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def al_cuadrado(n):
-	return n**2
+    return n**2
+
 
 # un commit
+# print("Gola")
---
+++ pytest.py
@@ -0,0 +1,13 @@
+import unittest
+import main
+
+
+class TestMyModule(unittest.TestCase):
+
+	def test_sum(self):
+		self.assertEqual(main.al_cuadrado(2), 4)
+		self.assertEqual(main.al_cuadrado(3),10)
+
+
+if __name__ == "__main__":
+	unittest.main()
