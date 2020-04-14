5858ce12043b32a37c436764adde9175764fdad4
deyren <deyren4@gmail.com>
2020-04-14 20:33:00 +0200

    otro masa

--- pytest.py
+++ pytest.py
@@ -6,7 +6,7 @@ class TestMyModule(unittest.TestCase):
 
 	def test_sum(self):
 		self.assertEqual(main.al_cuadrado(2), 4)
-		self.assertEqual(main.al_cuadrado(3),10)
+		self.assertEqual(main.al_cuadrado(3),9)
 
 
 if __name__ == "__main__":
